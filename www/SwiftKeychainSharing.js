2b552ef01cd2ce2ce7bb846f5dc26758cc583f48
Corey Kouns <coreykouns@pillarinnovations.com>
2017-03-14 14:16:53 -0400

    Copying swift file

--- www/SwiftKeychainSharing.js
+++ www/SwiftKeychainSharing.js
@@ -3,16 +3,8 @@ var exec = require('cordova/exec');
 var SwiftKeychainSharing = {
   serviceName: "SwiftKeychainSharing",
 
-  echo: function(arg0, success, error) {
-    exec(success, error, "ModusEchoSwift", "echo", [arg0]);
-  },
-
-  echojs: function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-      success(arg0);
-    } else {
-      error('Empty message!');
-    }
+  set: function(arg0, success, error) {
+    exec(success, error, "SwiftKeychainSharing", "set", [arg0]);
   }
 };
 
