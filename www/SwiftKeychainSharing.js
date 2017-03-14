d2619122e8816647db95d2e5920d090756a680da
Corey Kouns <coreykouns@pillarinnovations.com>
2017-03-14 13:31:06 -0400

    Refactoring js file to be more like originally used cordova keychain plugin

--- www/SwiftKeychainSharing.js
+++ www/SwiftKeychainSharing.js
@@ -1,13 +1,19 @@
 var exec = require('cordova/exec');
 
-exports.echo = function(arg0, success, error) {
-  exec(success, error, "ModusEchoSwift", "echo", [arg0]);
-};
+var SwiftKeychainSharing = {
+  serviceName: "SwiftKeychainSharing",
 
-exports.echojs = function(arg0, success, error) {
-  if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-    success(arg0);
-  } else {
-    error('Empty message!');
+  echo: function(arg0, success, error) {
+    exec(success, error, "ModusEchoSwift", "echo", [arg0]);
+  },
+
+  echojs: function(arg0, success, error) {
+    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+      success(arg0);
+    } else {
+      error('Empty message!');
+    }
   }
 };
+
+module.exports = SwiftKeychainSharing;
