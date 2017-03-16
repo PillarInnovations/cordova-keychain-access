1355b86bb7561a7ec9586c374e6a708e5becb940
Corey Kouns <coreykouns@pillarinnovations.com>
2017-03-16 15:01:18 -0400

    d

--- www/ModusEchoSwift.js
+++ www/ModusEchoSwift.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.test = function(arg0, success, error) {
   exec(success, error, 'ModusEchoSwift', 'test', [arg0]);
 };
+
+exports.set = function(arg0, success, error) {
+  exec(success, error, 'ModusEchoSwift', 'set', [arg0]);
+}
