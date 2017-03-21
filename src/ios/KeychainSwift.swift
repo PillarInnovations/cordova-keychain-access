94fe621b79b66b20ace2f78ee7349f656d69930d
Corey Kouns <coreykouns@pillarinnovations.com>
2017-03-21 10:48:08 -0400

    d

--- src/ios/KeychainSwift.swift
+++ src/ios/KeychainSwift.swift
@@ -24,15 +24,9 @@
       status: CDVCommandStatus_ERROR
     )
 
-    let obj = KeychainSwift()
-    let reflected = reflect(obj)
-    var members = [String: String]()
-    for index in 0..<reflected.count {
-       members[reflected[index].0] = reflected[index].1.summary
-    }
-    println(members)
-    //keychain.set("hello world", forKey: "testKey")
-    //let result = keychain.get("testKey")
+    let keychain = KeychainSwift()
+    keychain.set("hello world", forKey: "testKey")
+    let result = keychain.get("testKey")
     let result = "dood"
 
     pluginResult = CDVPluginResult(
