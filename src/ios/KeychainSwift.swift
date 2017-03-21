a1ab015d60cc65bc0a69b66eab62274aea4e7754
Corey Kouns <coreykouns@pillarinnovations.com>
2017-03-21 13:29:23 -0400

    Mapping to other functions in javascript

--- src/ios/KeychainSwift.swift
+++ src/ios/KeychainSwift.swift
@@ -39,4 +39,86 @@
       callbackId: command.callbackId
     )
   }
+
+  @objc(delete:)
+  func delete(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let keychain = KeychainSwift()
+    keychain.delete(command.arguments[0] as! String)
+    let result = "Successfully deleted key \(command.arguments[0])"
+
+    pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: result
+    )
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+
+  @objc(clear:)
+  func clear(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let keychain = KeychainSwift()
+    keychain.clear()
+    let result = "Successfully cleared all items from Keychain"
+
+    pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: result
+    )
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+
+  @objc(getBool:)
+  func getBool(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let keychain = KeychainSwift()
+    let result = keychain.getBool(command.arguments[0] as! String)
+
+    pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: result
+    )
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+
+  @objc(getData:)
+  func getData(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let keychain = KeychainSwift()
+    let result = keychain.getData(command.arguments[0] as! String)
+
+    pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_OK,
+      messageAs: result
+    )
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
 }
--- www/KeychainSwift.js
+++ www/KeychainSwift.js
@@ -7,3 +7,21 @@ exports.set = function(value, key, success, error) {
 exports.get = function(key, success, error) {
   exec(success, error, 'PillarKeychainSwift', 'get', [key]);
 };
+
+exports.delete = function(key, success, error) {
+  exec(success, error, 'PillarKeychainSwift', 'delete', [key]);
+};
+
+exports.clear = function(success, error) {
+  exec(success, error, 'PillarKeychainSwift', 'clear');
+};
+
+exports.getBool = function(key, success, error) {
+  exec(success, error, 'PillarKeychainSwift', 'getBool', [key]);
+};
+
+exports.getData = function(key, success, error) {
+  exec(success, error, 'PillarKeychainSwift', 'getData', [key]);
+};
+
+//pass in params for synchronizable, withAccess, accessGroup, keyPrefix
